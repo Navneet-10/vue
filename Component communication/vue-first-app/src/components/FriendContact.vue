08508768b882cecc860cb60e7a64c592cc52d38f
Navneet <navneetanand256@gmail.com>
2024-04-17 06:16:57 +0530

    adding more resources

---
+++ Component communication/vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+    <li>
+        <h2>{{ name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+            <li><strong>Email:</strong>{{ emailAddress }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    props: [
+        'name',
+        'phoneNumber',
+        'emailAddress'
+    ],
+    data() {
+        return {
+            detailsAreVisible: true,
+            // friend: {
+            //     id: 'pragnya',
+            //     name: 'Pragnya YS',
+            //     phone: '9999999999',
+            //     email: 'pragnya@vue.com'
+            // }
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+
+    
+};
+</script>
---
+++ Vue CLI and Development setup/vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: true,
+            friend: {
+                id: 'pragnya',
+                name: 'Pragnya YS',
+                phone: '9999999999',
+                email: 'pragnya@vue.com'
+            }
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+
+    
+};
+</script>
